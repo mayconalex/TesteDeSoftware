f88cb02d601cd61539cb3a09072f8aae50b4fa4d
Maycon Alexsander da Silva <mayconalexsander@hotmail.com>
2025-10-23 22:40:51 -0300

    :test_tube: test: cria mutantes

---
+++ mutante_02.py
@@ -0,0 +1,15 @@
+def classifica_triangulo(a, b, c):
+    if not all(isinstance(lado, (int, float)) for lado in [a, b, c]):
+        return "Lados devem ser numéricos."
+    if any(lado <= 0 for lado in [a, b, c]):
+        return "Lados devem ser positivos."
+
+    if a + b > c or a + c > b or b + c > a:
+        if a == b and b == c:
+            return "Equilátero"
+        elif a == b or b == c or a == c:
+            return "Isósceles"
+        else:
+            return "Escaleno"
+    else:
+        return "Não é um triângulo"
