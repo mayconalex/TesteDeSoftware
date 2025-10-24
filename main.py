7c2cd0859efedfb0197e5c9a1be16006abda3107
Maycon Alexsander da Silva <mayconalexsander@hotmail.com>
2025-10-23 22:27:08 -0300

    :recycle: refactor: isolamento de lógica

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-a = float(input())
-b = float(input())
-c = float(input())
+def classifica_triangulo(a, b, c):
+    if not all(isinstance(lado, (int, float)) for lado in [a, b, c]):
+        return "Lados devem ser numéricos."
+    if any(lado <= 0 for lado in [a, b, c]):
+        return "Lados devem ser positivos."
 
-if a + b > c and a + c > b and b + c > a:
-    if a == b == c:
-        print("Equilátero.")
-    elif a == b or b == c or a == c:
-        print("Isósceles.")
+    if a + b > c and a + c > b and b + c > a:
+        if a == b and b == c:
+            return "Equilátero"
+        elif a == b or b == c or a == c:
+            return "Isósceles"
+        else:
+            return "Escaleno"
     else:
-        print("Escaleno.")
-else:
-    print("Não é um triângulo.")
+        return "Não é um triângulo"
