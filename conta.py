95b4f9f9d2441be6bd457d1f95ef49858d944a8a
Maycon Alexsander da Silva <mayconalexsander@hotmail.com>
2026-01-18 15:26:29 -0300

    conta bancária com TDD

---
+++ conta.py
@@ -0,0 +1,11 @@
+class ContaBancaria:
+    def __init__(self):
+        self.saldo = 0
+
+    def depositar(self, valor):
+        self.saldo += valor
+
+    def sacar(self, valor):
+        if self.saldo < valor:
+            return False
+        self.saldo -= valor
