e874ca2071fc116927071a25753d7819f5454f57
Maycon Alexsander da Silva <mayconalexsander@hotmail.com>
2026-01-18 13:37:52 -0300

    fibonacci com TDD

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
 import unittest
+from main import fib
+
+
+class TestFibonacci(unittest.TestCase):
+    def test_fib(self):
+        casos = [
+            (0, 0),
+            (1, 1),
+            (2, 1),
+            (3, 2),
+        ]
+
+        for n, expected in casos:
+            with self.subTest(n=n):
+                self.assertEqual(fib(n), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
